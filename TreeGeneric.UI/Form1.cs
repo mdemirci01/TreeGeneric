7ae3bae7e9c8545e528fea2ab0d230bd4352184b
Murat Demirci <mdemirci@outlook.com>
2019-01-29 14:42:43 +0300

    first version

---
+++ TreeGeneric.UI/Form1.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TreeGeneric.BussinessLogic;
+
+namespace TreeGeneric.UI
+{
+    public partial class Form1 : Form
+    {
+        private readonly ILifetimeScope scope;
+        private readonly IRegionService regionService;
+        public Form1(ILifetimeScope scope)
+        {
+            this.scope = scope;
+            this.regionService = scope.Resolve<IRegionService>();
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.comboBox1.DataSource = regionService.GetAll();
+        }
+    }
+}
---
+++ TreeGeneric.UI/Program.cs
@@ -0,0 +1,40 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TreeGeneric.BussinessLogic;
+using TreeGeneric.Data;
+using TreeGeneric.Model;
+
+namespace TreeGeneric.UI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // Autofac = IoC (Inversion-of-Control) Provider'ıdır. Bununla dependency injection işlemi kolaylaşır.
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ApplicationDbContext>().As<ApplicationDbContext>();            
+
+            builder.RegisterGeneric(typeof(Repository<>)).As(typeof(IRepository<>));
+
+            builder.RegisterType<RegionService>().As<IRegionService>();
+            builder.RegisterType<PlantingService>().As<IPlantingService>();
+
+            var container = builder.Build();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1(scope));
+            }
+        }
+    }
+}
